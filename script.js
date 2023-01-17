521f1ee7d80f1ae829e33ca46fae62b7455d9805
Caitlin Millar <caitlin-millar@outlook.com>
2023-01-17 14:50:30 +0000

    begun adding button/submit queries

---
+++ script.js
@@ -0,0 +1,23 @@
+//grabbing enter button
+const button = document.querySelector('#enter');
+
+
+//function
+const submitted = (event) => {
+    
+    event.preventDefault();
+    //creating a new list element
+    const createListEl = document.createElement("li");
+    createListEl.textContent = event.target.newtodo.value;
+
+    //grab new element
+    const newtodo = document.getElementById('#list');
+
+    newtodo.appendChild(createListEl);
+    console.log("button clicked");
+}
+//adding event listener
+button.addEventListener('click', submitted);
+
+// Form.addEventListener('submit', doSubmit);
+
