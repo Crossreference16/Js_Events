552d884d77de04a9c9569af373d5b8506d166638
Georgia Clarke <georgiaclarke@Georgias-MacBook-Pro.local>
2023-01-17 15:22:05 +0000

    trying to fix bug

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //grabbing enter button
 const button = document.querySelector('#enter');
 
+const todoForm = document.querySelector('.form');
 
 //function
 const submitted = (event) => {
@@ -9,15 +10,18 @@ const submitted = (event) => {
     //creating a new list element
     const createListEl = document.createElement("li");
     createListEl.textContent = event.target.newtodo.value;
-
+    
     //grab new element
-    const newtodo = document.getElementById('#list');
-
+    const newtodo = document.getElementById('list');
+    
     newtodo.appendChild(createListEl);
     console.log("button clicked");
+
+    // button.addEventListener('click', submitted);
 }
 //adding event listener
-button.addEventListener('click', submitted);
+
+ button.addEventListener('click', submitted);
 
 // Form.addEventListener('submit', doSubmit);
 
