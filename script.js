0b5a3ad810fe6dd22519451aec3d0b1f1753066d
Caitlin Millar <caitlin-millar@outlook.com>
2023-01-17 16:29:52 +0000

    delet button sort of workd/not really

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 //grabbing enter button
 const button = document.querySelector('#enter');
-
+const deleteButton = document.createElement("button");
 
 //function
 const submitted = (event) => {
-    
     event.preventDefault();
     //creating a new list element
     const createListEl = document.createElement("li");
     const value = document.getElementById("newtodo").value;
+   //delete button
+
+    deleteButton.innerHTML = "Delete";
+    alert(createListEl);
+    deleteButton.setAttribute("class", "deleteButton");
 
     createListEl.textContent = value;
     console.log(event);
@@ -17,9 +21,16 @@ const submitted = (event) => {
     const newtodo = document.getElementById('list');
 
     newtodo.appendChild(createListEl);
+    createListEl.appendChild(deleteButton);
     console.log("button clicked");
+        document.querySelector("input").value="";
 }
+
+
 //adding event listener
+ deleteButton.addEventListener("click", function(event){
+    newtodo.removeChild(li);
+})
 button.addEventListener('click', submitted);
 
 // Form.addEventListener('submit', doSubmit);
