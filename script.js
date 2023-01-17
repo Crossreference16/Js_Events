f700dc862905bedc1d55cae5f973e51a07696c59
Khalil <khalil.hersi@hotmail.co.uk>
2023-01-17 15:58:08 +0000

    adds functionality to take input/add to a list

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //grabbing enter button
 const button = document.querySelector('#enter');
 
-const todoForm = document.querySelector('.form');
 
 //function
 const submitted = (event) => {
@@ -9,19 +8,19 @@ const submitted = (event) => {
     event.preventDefault();
     //creating a new list element
     const createListEl = document.createElement("li");
-    createListEl.textContent = event.target.newtodo.value;
+    const value = document.getElementById("newtodo").value;
+
+    createListEl.textContent = value;
+    console.log(event);
     
     //grab new element
     const newtodo = document.getElementById('list');
-    
+
     newtodo.appendChild(createListEl);
     console.log("button clicked");
-
-    // button.addEventListener('click', submitted);
 }
 //adding event listener
-
- button.addEventListener('click', submitted);
+button.addEventListener('click', submitted);
 
 // Form.addEventListener('submit', doSubmit);
 
